7df58ac1a41e4793167060740e4fce13c2164965
ppp <974270397@qq.com>
2019-10-23 15:47:20 +0800

    初始化

---
+++ index.ts
@@ -0,0 +1,16 @@
+import * as http from "http";
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    console.log('有人请求了')
+    response.end('hi')
+})
+
+server.listen(8888)
+
+
+
+
+
+
